3e3ee61b65b6bf0c8890f998c4e062174a8c90cf
blared00 <yudintsev.vsevolod@gmail.com>
2021-09-02 07:32:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+class CreditCalculator:
+    """Калькулятор для расчета выплат по кредиту"""
+    def __init__(self, request_str):
+        pass
+
+    def get_parametrs(self, request_str):
+        pass
